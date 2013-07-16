fe52bd844659feb633c83fb691558ded042b276f
graham <graham@wellnessfx.com>
2013-07-16 16:11:53 -0700

    Allow deploying from a detached head
    Allows deploying from git version 1.7.5 from a detached head
    See http://git.661346.n2.nabble.com/error-with-git-push-origin-HEAD-newbranch-td6333596.html

--- lib/heroku-headless/deployer.rb
+++ lib/heroku-headless/deployer.rb
@@ -97,7 +97,7 @@ module HerokuHeadless
     def git_push_command
       cmd = "git push "
       cmd << "-f " if HerokuHeadless.configuration.force_push
-      cmd << "git@heroku.com:#{@app_name}.git HEAD:master"
+      cmd << "git@heroku.com:#{@app_name}.git HEAD:refs/heads/master"
     end
 
     def run_pre_deploy_git_commands
